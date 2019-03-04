3103f704feebb56819dc6ab22f21db5eb3153a41
Amirahmad Khordadi <amirahmadkhordadi@gmail.com>
2019-03-04 10:34:36 +0330

    add some sensors

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,131 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        setupProximitySensor(sensorManager);
+        setupGyroscopeSensor(sensorManager);
+        setupRotationVectorSensor(sensorManager);
+
+    }
+
+    private void setupRotationVectorSensor(SensorManager sensorManager) {
+        Sensor rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        // Create a listener
+        SensorEventListener rvListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(
+                        rotationMatrix, sensorEvent.values);
+
+                // Remap coordinate system
+                float[] remappedRotationMatrix = new float[16];
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z,
+                        remappedRotationMatrix);
+
+                // Convert to orientations
+                float[] orientations = new float[3];
+                SensorManager.getOrientation(remappedRotationMatrix, orientations);
+
+                for (int i = 0; i < 3; i++) {
+                    orientations[i] = (float) (Math.toDegrees(orientations[i]));
+                }
+
+                if (orientations[2] > 45) {
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                } else if (orientations[2] < -45) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (Math.abs(orientations[2]) < 10) {
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        // Register it
+        sensorManager.registerListener(rvListener,
+                rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void setupGyroscopeSensor(SensorManager sensorManager) {
+        Sensor gyroscopeSensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        // Create a listener
+        SensorEventListener gyroscopeSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent.values[2] > 0.5f) { // anticlockwise
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (sensorEvent.values[2] < -0.5f) { // clockwise
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        // Register the listener
+        sensorManager.registerListener(gyroscopeSensorListener,
+                gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void setupProximitySensor(SensorManager sensorManager) {
+        final Sensor proximitySensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (proximitySensor == null) {
+            Log.e("error", "Proximity sensor not available.");
+            finish(); // Close app
+        }
+
+        // Create listener
+        SensorEventListener proximitySensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent.values[0] < Objects.requireNonNull(proximitySensor).getMaximumRange()) {
+                    // Detected something nearby
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                } else {
+                    // Nothing is nearby
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        // Register it, specifying the polling interval in microseconds
+        sensorManager.registerListener(proximitySensorListener, proximitySensor, 2 * 1000 * 1000);
     }
 }
